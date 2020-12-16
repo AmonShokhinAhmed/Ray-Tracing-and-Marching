43267e71cbe4f66c183c3f9e2a79ee367892666f
Amon Shokhin Ahmed <amon.shokhin.ahmed@gmail.com>
2020-12-16 13:56:17 +0100

    Add antialiasing

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -4,7 +4,19 @@ public class RayTracingMaster : MonoBehaviour
     public Texture SkyboxTexture;
     public ComputeShader RayTracingShader;
     private RenderTexture _target; 
-    private Camera _camera;
+    private Camera _camera; 
+    
+    private uint _currentSample = 0;
+    private Material _addMaterial;
+
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
 
     private void Awake()
     {
@@ -20,7 +32,8 @@ public class RayTracingMaster : MonoBehaviour
     {
         RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
         RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse); 
-        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture); 
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
     }
     private void Render(RenderTexture destination)
     {
@@ -32,7 +45,12 @@ public class RayTracingMaster : MonoBehaviour
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
         RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
         // Blit the result texture to the screen
-        Graphics.Blit(_target, destination);
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, destination, _addMaterial);
+        _currentSample++;
+        _currentSample%=24;
     }
     private void InitRenderTexture()
     {
