effe34dc681c6fc7896756194dbebf0f9e2752d8
Amon Shokhin Ahmed <amon.shokhin.ahmed@gmail.com>
2020-12-16 15:25:43 +0100

    Add materials.

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -1,39 +1,101 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 public class RayTracingMaster : MonoBehaviour
 {
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    }
     public Texture SkyboxTexture;
     public ComputeShader RayTracingShader;
-    private RenderTexture _target; 
-    private Camera _camera; 
-    
+    private RenderTexture _target;
+    private Camera _camera;
+
     private uint _currentSample = 0;
     private Material _addMaterial;
 
-    private void Update()
-    {
-        if (transform.hasChanged)
-        {
-            _currentSample = 0;
-            transform.hasChanged = false;
-        }
-    }
+    public Light DirectionalLight;
 
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    private ComputeBuffer _sphereBuffer;
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
     private void Awake()
     {
         _camera = GetComponent<Camera>();
     }
-   
+    private void Update()
+    {
+        if (transform.hasChanged || DirectionalLight.transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+            DirectionalLight.transform.hasChanged = false;
+        }
+    }
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         SetShaderParameters();
         Render(destination);
     }
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+        // Add a number of random spheres
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+            // Radius and radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+            // Add the sphere to the list
+            spheres.Add(sphere);
+        SkipSphere:
+            continue;
+        }
+        // Assign to compute buffer
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+    }
     private void SetShaderParameters()
     {
         RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
-        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse); 
-        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture); 
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
         RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+
     }
     private void Render(RenderTexture destination)
     {
@@ -50,7 +112,6 @@ public class RayTracingMaster : MonoBehaviour
         _addMaterial.SetFloat("_Sample", _currentSample);
         Graphics.Blit(_target, destination, _addMaterial);
         _currentSample++;
-        //_currentSample%=24;
     }
     private void InitRenderTexture()
     {
